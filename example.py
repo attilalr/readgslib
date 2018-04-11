88fac45873ec5bc51c3ad96752b442f983d1f515
Áttila Leães Rodrigues <attila.leaes@gmail.com>
2018-04-11 14:37:13 -0300

    New write class. readgslib is now 'writegslib' too.

--- example.py
+++ example.py
@@ -1,5 +1,7 @@
 import readgslib
+import numpy as np
 
+### read class #
 a = readgslib.read('test_data.gslib')
 
 print (a)
@@ -28,3 +30,25 @@ a.replace_nan(0)
 # get full matrix data
 print (a.matrix())
 
+### write class #
+
+b = readgslib.write()
+
+b.set_title('Title test')
+v1 = [3,2,4]
+v2 = np.array([3,2,4])
+
+b.put_var('Var1', v1)
+
+b.put_var('VAR2', v2)
+
+b.write('test_gslib.dat')
+
+# the file will be (without '#'):
+#Title test
+#2
+#Var1
+#VAR2
+#3 3
+#2 2
+#4 4
--- readgslib.py
+++ readgslib.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os.path
 
 class read:
   def __init__(self, filename, verbose=0, nan=None):
@@ -69,6 +70,97 @@ class read:
     
   def __str__(self):
     return '<Obj. from Gslib data, cols: '+', '.join(self.cols)+', data matrix shape: '+str((self.m).shape)+'>'
+
+
+### write class
+
+class write:
+  def __init__(self, verbose=0, nan=None):
+    self.clear()
+    self.set_nan(nan)
     
-#  def __repr__(self):
-#    return __str__()
+  def clear(self):
+    self.filename = None
+    self.ncols = None
+    self.cols = list()
+    self.title = None
+    self.rows = None
+    self.m = None
+    self.dict_data = dict()
+    
+  def set_nan(self, nan):
+    self.nan = nan
+
+  def set_title(self, title):
+    self.title = title
+    
+  def put_var(self, name, vec):
+    # check if there is data already in the matrix
+    if type(self.m) == type(None):
+      vec = np.squeeze(np.array(vec))
+      if len(vec.shape) > 1:
+        # error, input vector with dim > 1
+        pass
+      else:
+        nrows = vec.size
+        self.rows = nrows
+        self.cols.append(name)
+        self.m = vec.reshape((vec.size, 1))
+        self.ncols = 1
+        self.dict_data[name] = self.m[:, -1]
+    else:
+      # if theres is data already we must to check the row number
+      vec = np.squeeze(np.array(vec))
+      if len(vec.shape) > 1:
+        # error, input vector with dim > 1
+        pass
+      else:
+        nrows = vec.size
+        if nrows != self.rows:
+          print ('Wrong number of rows for variable '+str(name))
+          # exit
+        else:         
+          self.cols.append(name)
+          self.m = np.hstack((self.m, vec.reshape(vec.size, 1)))
+          self.dict_data[name] = self.m[:, -1]
+          self.ncols = self.ncols + 1
+  
+  def write(self, filename, overwrite=False):
+    self.filename = filename
+  
+    if os.path.isfile(self.filename) and overwrite == False:
+      # dont write
+      print ('File exist. Use parameter overwrite=True to overwrite file.')
+    else:
+      # write file
+      if self.title == None:
+        self.title = 'Dummy Title'
+      string = self.title+'\n'+str(self.ncols)+'\n'
+      for i, col in enumerate(self.cols):
+        string = string + col
+        if i != len(self.cols)-1:
+          string = string + '\n'
+      np.savetxt(self.filename, self.m, header=string, delimiter=' ', fmt='%s', comments='')
+
+  def cols(self):
+    return self.cols
+    
+  def col(self, col_item):
+    if type(col_item) == type('a'):
+      return self.dict_data[col_item]
+    elif type(col_item) == type(1):
+      return self.m[:,col_item]
+    
+  def matrix(self):
+    return self.m
+    
+  def replace_nan(self, x, col=None):
+    if col == None:
+      self.m[self.m == self.nan] = x
+    else:
+      b = self.m[:, col]
+      b[b==self.nan] = x
+    
+  def __str__(self):
+    return '<Obj. to writ Gslib data, cols: '+', '.join(self.cols)+', data matrix shape: '+str((self.m).shape)+'>'
+
